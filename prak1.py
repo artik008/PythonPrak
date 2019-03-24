2ae890c089608fc7505e5496fb897ca8e52e9c7c
artik008 <artik008@yandex.ru>
2019-03-24 14:29:54 +0300

    Three buttons

--- prak1.py
+++ prak1.py
@@ -3,16 +3,27 @@ from tkinter import *
 
 root = Tk()
 
-btn = Button(root,                  
+def print1(event):
+  print("1")
+
+def print2(event):
+  print("2")
+
+
+btn1 = Button(root,                  
              text="Закрыть",       
              width=30,height=5,     
-             bg="white",fg="black") 
+             bg="white",fg="black")
+btn1.bind("<Button-1>", exit)      
+btn1.pack()  
 
-btn.bind("<Button-1>", exit)       
-btn.pack()                
+btn2 = Button(root, text="1")
+btn2.bind("<Button-1>", print1)       
+btn2.pack(side = 'right')   
+
+btn3 = Button(root, text="2") 
+btn3.bind("<Button-1>", print2)    
+btn3.pack(side = 'left')   
 
 def main():
   root.mainloop()
-
-if __name__ == '__main__':
-    main()
